f4e3abae46fa1768689b4f2e2f9754485a97a3ff
artoo <artoo@artixlinux.org>
2022-04-11 02:06:20 +0200

    buildiso: clean up s6 sv handling

--- lib/iso/services.sh
+++ lib/iso/services.sh
@@ -24,26 +24,25 @@ add_svc_runit(){
 
 add_svc_s6(){
     local mnt="$1" names="$2" rlvl="${3:-default}" dep
+    dep="$mnt"/etc/s6/sv/"$display_manager"-srv/dependencies.d
     for svc in $names; do
         msg2 "Setting %s: [%s]" "${INITSYS}" "$svc"
-#         touch "$mnt"/etc/s6/adminsv/default/contents.d/"$svc"
         chroot "$mnt" s6-service add "$rlvl" "$svc"
         if [[ "$svc" == "$display_manager" ]]; then
-            dep="$mnt"/etc/s6/sv/"$display_manager"-srv/dependencies.d
             if [[ -d "$dep" ]]; then
                 touch "$dep"/artix-live
             fi
         fi
     done
 
-    local rlvl=/etc/s6/current
-    # rebuild s6-linux-init binaries
-    chroot "$mnt" rm -r "$rlvl"
-    chroot "$mnt" s6-linux-init-maker -1 -N -f /etc/s6/skel -G "/usr/bin/agetty -L -8 tty7 115200" -c "$rlvl" "$rlvl"
-    chroot "$mnt" mv "$rlvl"/bin/init "$rlvl"/bin/s6-init
-    chroot "$mnt" cp -a "$rlvl"/bin /usr
-
     chroot "$mnt" s6-db-reload -r
+
+    local src=/etc/s6/current skel=/etc/s6/skel getty='/usr/bin/agetty -L -8 tty7 115200'
+    # rebuild s6-linux-init binaries
+    chroot "$mnt" rm -r "$src"
+    chroot "$mnt" s6-linux-init-maker -1 -N -f "$skel" -G "$getty" -c "$src" "$src"
+    chroot "$mnt" mv "$src"/bin/init "$src"/bin/s6-init
+    chroot "$mnt" cp -a "$src"/bin /usr
 }
 
 add_svc_suite66(){
